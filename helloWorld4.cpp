a3d7a013922e310e47b9e0b3d3614358856b415f
sonic11829 <sethneal15@ymail.com>
2023-07-25 20:05:59 -0400

    completed helloWorld4.cpp

---
+++ helloWorld4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+//typedef std::vector<std::pair<std::string, int>> pairlist_t;
+//typedef std::string text_t;
+//typedef int number_t;
+using text_t = std::string;
+using number_t = int;
+
+int main(){
+
+  text_t firstName = "Seth";
+  number_t age = 30;
+
+  std::cout << firstName << '\n';
+  std::cout << age << '\n';
+
+  return 0;
+}
