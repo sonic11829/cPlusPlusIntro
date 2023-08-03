72dfa85a5cb57908fc4592953b902be28cf38d6c
sonic11829 <sethneal15@ymail.com>
2023-08-02 20:48:10 -0400

    finished lesson6 helloWorld6.cpp

--- lesson6/helloWorld6.cpp
+++ lesson6/helloWorld6.cpp
@@ -7,9 +7,20 @@ int main(){
 
   // int x = 3.14;  when displayed shows 3 because it is stored into an integer
   // double x = 3.14; when displayed shows 3.14 because it is stored as a double
-  double x = (int) 3.14; // when displayed shows 3 because 3.14 is made an integer then stored as a double
+  // double x = (int) 3.14; when displayed shows 3 because 3.14 is made an integer then stored as a double
+  // std::cout << x;
 
-  std::cout << x;
+  // char x = 100;
+
+  // std::cout << x; // will show d from the ASCII Table
+  // std::cout << (char)100;
+
+  int correct = 8; // example of implicit cause I am Implicitly making correct an intiger
+  int questions = 10; // example of implicit
+  double score = correct/(double)questions * 100; // questions is being used "explicitly" as double because without 
+  // double it would be 8 / 10 which is 0.8, so it would be trunkated to 0, then multiplied by 100 whit is 0
+
+  std::cout << score << "%";
 
   return 0;
 }
