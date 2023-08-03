7e6f719ceca1ac48a9944d3b554058c33e98c5d6
sonic11829 <sethneal15@ymail.com>
2023-08-02 20:33:21 -0400

    added in notes

---
+++ lesson6/helloWorld6.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+  /* type conversion = conversion a value of one data type to another
+          Implicit = automatic
+          Explicit = Precede value with new data type (int) */
+
+  return 0;
+}
