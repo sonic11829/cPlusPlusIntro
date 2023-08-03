e8cc5d8c2ff2b01155996c8a46f97b293cae294f
sonic11829 <sethneal15@ymail.com>
2023-08-02 20:37:10 -0400

    learned how to store a variable as a double that was made into an integer

--- lesson6/helloWorld6.cpp
+++ lesson6/helloWorld6.cpp
@@ -5,5 +5,11 @@ int main(){
           Implicit = automatic
           Explicit = Precede value with new data type (int) */
 
+  // int x = 3.14;  when displayed shows 3 because it is stored into an integer
+  // double x = 3.14; when displayed shows 3.14 because it is stored as a double
+  double x = (int) 3.14; // when displayed shows 3 because 3.14 is made an integer then stored as a double
+
+  std::cout << x;
+
   return 0;
 }
