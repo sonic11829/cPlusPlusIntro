f94502b061863129c29f91fdbfc8f3e0de85f2a0
sonic11829 <sethneal15@ymail.com>
2023-08-08 22:11:12 -0400

    learned max and min

--- lesson8/helloWorld8.cpp
+++ lesson8/helloWorld8.cpp
@@ -2,8 +2,14 @@
 
 int main(){
 
+  double x = 3;
+  double y = 4;
+  double z;
 
-  
+  // z = std::max(x, y); // max displays the highest value, in this case 4 because y = 4.
+  // z = std::min(x, y); // min displays the lowest value, in this case 3 because x = 3.
+
+  std::cout << z;  
 
   return 0;
 }
