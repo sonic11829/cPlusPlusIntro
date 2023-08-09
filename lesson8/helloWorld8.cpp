42b9c10fd905f4cf7bf457905acbc2031294d8e5
sonic11829 <sethneal15@ymail.com>
2023-08-08 22:20:30 -0400

    learned round & ceil

--- lesson8/helloWorld8.cpp
+++ lesson8/helloWorld8.cpp
@@ -3,7 +3,7 @@
 
 int main(){
 
-  double x = 3;
+  double x = 3.14;
   double y = 4;
   double z;
 
@@ -13,6 +13,8 @@ int main(){
                  // In this case it's 2 * 2 * 2 * 2 or 2 to the power of 4 which is equal to 16.
   // z = sqrt(9); // sqrt or square-root displays the square root of the number passed into the parrenthesis.
   // z = abs(-3); // abs or absolute value, if you pass in a negative value you will be given the positive version of that number
+  // z = round(x); // round round a number to the closest whole number, in this case it will be 3 because 3.14 is closes to 3.
+  z = ceil(x); // ceil or ceiling rounds a number up to the closes whole number, in this case 4.
 
   std::cout << z;  
 
