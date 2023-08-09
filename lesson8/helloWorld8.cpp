be98c5891b33d3f08650537a8705e168e3fbfb77
sonic11829 <sethneal15@ymail.com>
2023-08-08 22:27:16 -0400

    completed lesson8 and updated tableOfContents.txt

--- lesson8/helloWorld8.cpp
+++ lesson8/helloWorld8.cpp
@@ -3,7 +3,9 @@
 
 int main(){
 
-  double x = 3.14;
+  // double x = 3;
+  // double x = 3.14;
+  double x = 3.99;
   double y = 4;
   double z;
 
@@ -14,7 +16,8 @@ int main(){
   // z = sqrt(9); // sqrt or square-root displays the square root of the number passed into the parrenthesis.
   // z = abs(-3); // abs or absolute value, if you pass in a negative value you will be given the positive version of that number
   // z = round(x); // round round a number to the closest whole number, in this case it will be 3 because 3.14 is closes to 3.
-  z = ceil(x); // ceil or ceiling rounds a number up to the closes whole number, in this case 4.
+  // z = ceil(x); // ceil or ceiling rounds a number up to the closes whole number, in this case 4.
+  z = floor(x); // floor rounds a number down to the closes whole number, in this case 3.
 
   std::cout << z;  
 
