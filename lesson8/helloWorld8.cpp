683baa3a480ba49cc41590ca79697baee2ddf5fc
sonic11829 <sethneal15@ymail.com>
2023-08-08 22:17:27 -0400

    learned pow, sqrt, & abs

--- lesson8/helloWorld8.cpp
+++ lesson8/helloWorld8.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 int main(){
 
@@ -8,6 +9,10 @@ int main(){
 
   // z = std::max(x, y); // max displays the highest value, in this case 4 because y = 4.
   // z = std::min(x, y); // min displays the lowest value, in this case 3 because x = 3.
+  // z = pow(2, 4); // pow or power displays the hights power of the first number using the second number.
+                 // In this case it's 2 * 2 * 2 * 2 or 2 to the power of 4 which is equal to 16.
+  // z = sqrt(9); // sqrt or square-root displays the square root of the number passed into the parrenthesis.
+  // z = abs(-3); // abs or absolute value, if you pass in a negative value you will be given the positive version of that number
 
   std::cout << z;  
 
