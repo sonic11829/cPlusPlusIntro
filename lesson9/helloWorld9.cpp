2a410633a14d22229ea11a7e7b2e0a489ebab8c1
sonic11829 <sethneal15@ymail.com>
2023-08-09 22:32:10 -0400

    added in notes

--- lesson9/helloWorld9.cpp
+++ lesson9/helloWorld9.cpp
@@ -7,7 +7,7 @@ int main(){
   double b;
   double c;
 
-  std::cout << "Enter side A: ";
+  std::cout << "Enter side A: "; //These 4 lines of code are what allow the user to input a number into the equasion
   std::cin >> a;
 
   std::cout << "Enter side B: ";
@@ -19,11 +19,11 @@ int main(){
   //for the samething but less lines of code...
   c = sqrt(pow(a, 2) + pow(b, 2));
 
-  std::cout << "side C: " << c;
+  // std::cout << "side C: " << c;
 
   return 0;
 }
 
 /*                                                  _________
- the hypotenuse of a right angled triangle is c = \/a^2 + b^2   
+ the hypotenuse of a right angled triangle is c = \/a^2 + b^2   " 'c' = the squareroot of 'a' squared + 'b' squared"
  */
