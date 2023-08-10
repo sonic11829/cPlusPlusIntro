b53ae44a51042e2102bd9034eb35ef79666ab9c7
sonic11829 <sethneal15@ymail.com>
2023-08-09 22:28:49 -0400

    learned how to find the hypotenuse of a triangle

--- lesson9/helloWorld9.cpp
+++ lesson9/helloWorld9.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <cmath>
 
 int main(){
 
-  
+  double a;
+  double b;
+  double c;
+
+  std::cout << "Enter side A: ";
+  std::cin >> a;
+
+  std::cout << "Enter side B: ";
+  std::cin >> b;
+
+  // a = pow(a, 2);
+  // b = pow(b, 2);
+  // c = sqrt(a + b);
+  //for the samething but less lines of code...
+  c = sqrt(pow(a, 2) + pow(b, 2));
+
+  std::cout << "side C: " << c;
 
   return 0;
 }
+
+/*                                                  _________
+ the hypotenuse of a right angled triangle is c = \/a^2 + b^2   
+ */
